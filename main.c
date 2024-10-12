83aba421318ca6e70b75ab6fa2fac3e6b023c3da
Ivan Trepakov <liontiger23@gmail.com>
2024-10-12 11:36:10 +0700

    Fix bug

--- main.c
+++ main.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 
 int main() {
-
   while (1) {
     int n = 0;
     int res = scanf("%d", &n);
-    if (res < 0) {
+    if (res < 1) {
       break;
     }
     printf("%d\n", n * n);
